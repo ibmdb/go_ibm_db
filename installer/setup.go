f024955b7b4b7f937e5995030027c82b054431d7
John Allberg <john@ayoy.se>
2019-03-22 10:42:57 +0100

    Handle errors from io.Copy.

--- installer/setup.go
+++ installer/setup.go
@@ -107,7 +107,11 @@ func Untaring(sourcefile string) {
                     fmt.Println(err)
                     os.Exit(1)
                 }
-                io.Copy(writer, tarBallReader)
+				_, err = io.Copy(writer, tarBallReader)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
                 err = os.Chmod(filename, os.FileMode(header.Mode))
                 if err != nil {
                     fmt.Println(err)
