7280036d1d8709bb86e8bbb1877a77ab415e0a09
Santiago De la Cruz <51337247+xhit@users.noreply.github.com>
2020-02-06 16:41:20 -0400

    Avoid register the driver when panic
    If panic when driver init then recover and avoid register the driver.
    
    Fixes #57

--- driver.go
+++ driver.go
@@ -50,6 +50,14 @@ func (d *Driver) Close() error {
 }
 
 func init() {
+	
+	// Recover from panic to avoid stop an application when can't get the db2 cli
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(fmt.Sprintf("%s\nThe go_ibm_db driver cannot be registered", err))
+		}
+	}()
+	
 	err := initDriver()
 	if err != nil {
 		panic(err)
