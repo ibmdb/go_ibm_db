f6c6a03cb4082481b6b2d6b775edd49ec6fdce77
alejandro.labad <alejandro.labad.next@bbva.com>
2020-08-27 12:40:55 +0200

    Improved solution as it is done by sql.go

--- conn.go
+++ conn.go
@@ -48,22 +48,10 @@ func (c *Conn) Close() error {
 
 //Query method executes the statement directly if no params present, or as prepared statement
 func (c *Conn) Query(query string, args []driver.Value) (driver.Rows, error) {
-	if len(args) == 0 {
-		// Going to original implementation
-		return c.noParamsQuery(query)
-	} else {
-		// This part is implemented as the original Query method did not use the provided args
-		stmt, err := c.Prepare(query)
-		if err != nil {
-			return nil, err
-		}
-		defer stmt.Close()
-		return stmt.Query(args)
+	if len(args) > 0 {
+		// Not implemented for queries with parameters
+		return nil, driver.ErrSkip
 	}
-}
-
-//noParamsQuery method executes the statement without prepare
-func (c *Conn) noParamsQuery(query string) (driver.Rows, error) {
 	var out api.SQLHANDLE
 	var os *ODBCStmt
 	ret := api.SQLAllocHandle(api.SQL_HANDLE_STMT, api.SQLHANDLE(c.h), &out)
